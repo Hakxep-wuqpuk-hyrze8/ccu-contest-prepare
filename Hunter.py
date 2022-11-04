0d5a0af1b7667ed914e8f384108afe17cc2b07b3
Onandon <65276244+Hakxep-wuqpuk-hyrze8@users.noreply.github.com>
2022-11-04 19:28:54 +0800

    Delete Hunter.py

--- Hunter.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-response = requests.get('https://www.yda.gov.tw/')
-soup = BeautifulSoup(response.content, 'lxml')
-dates = soup.find_all("small", class_="date")
-for date in dates:
-    date_text = date.text.replace(' ', '').replace('\n', '')
-    print(date_text)
